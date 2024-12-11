e7305e31938577c814f7c26de6e9011e95b5575b
DCGercossimo <dcgercossimo@gmail.com>
2024-12-10 21:17:51 -0300

    first commit

---
+++ src/main/java/br/com/aurealab/aurealab_api/AurealabApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.aurealab.aurealab_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurealabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AurealabApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aurealab/aurealab_api/AurealabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aurealab.aurealab_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurealabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
