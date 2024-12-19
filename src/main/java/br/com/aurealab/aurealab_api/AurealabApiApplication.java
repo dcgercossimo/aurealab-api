58ae0cadb047648879a2b4188875f4685f7e5500
DCGercossimo <dcgercossimo@gmail.com>
2024-12-19 11:00:09 -0300

    code: format files with `spotless`

--- src/main/java/br/com/aurealab/aurealab_api/AurealabApiApplication.java
+++ src/main/java/br/com/aurealab/aurealab_api/AurealabApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AurealabApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AurealabApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AurealabApiApplication.class, args);
+    }
 }
